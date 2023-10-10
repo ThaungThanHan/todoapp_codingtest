b2dcdaa1b404bfce0f21514aba3b717989c833e5
ThaungThanHan <ericlimmm115@gmail.com>
2023-10-11 01:03:23 +0700

    sampled.env added, fixes some designs

--- src/app/components/userInfo.tsx
+++ src/app/components/userInfo.tsx
@@ -1,6 +1,6 @@
 "use client";
 import "../styles/app.scss";
-import React, {useState,useEffect} from "react";
+import React from "react";
 import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
 import {toast} from 'react-hot-toast';
